99e2342bb975bd73ffd490a7b95ddf4024247d71
Wojciech Dzieciol <gravity.wjd@gmail.com>
2026-07-08 16:18:38 +0200

    fix: (migration_runner) struct type name fixed

--- internal/db/migration_runner.go
+++ internal/db/migration_runner.go
@@ -2,6 +2,6 @@ package db
 
 // =====================================
 // Specify all migrations IN VALID ORDER
-// Migrate me type: func(context.Context, *pgxpool.Pool, string) error
+// MigrateMe type (delegate): func(context.Context, *pgxpool.Pool, string) error
 // =====================================
 var Migrations = []MigrateMe{RunCoreMigration260624, RunDomainMigration260624}
